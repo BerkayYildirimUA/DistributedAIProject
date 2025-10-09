533458242672e16b16ce937c148e6de093ce0749
WarreH <warrehofmans@gmail.com>
2025-10-09 11:56:37 +0200

    optimised yolo code

--- yolo_experiment/carla_validate_yolo.py
+++ yolo_experiment/carla_validate_yolo.py
@@ -5,11 +5,15 @@ import carla
 import numpy as np
 import cv2
 import onnxruntime as ort
+import torch
+import torchvision
+
 
 # https://medium.com/@zain.18j2000/how-to-use-your-yolov11-model-with-onnx-runtime-69f4ea243c01
 # TODO: remove overlapping boxes
 class ObjectDetection:
     def __init__(self):
+        # Initialize model
         # CUDAExecutionProvider
         providers = ["CPUExecutionProvider"]
         self.session = ort.InferenceSession("updated_model.onnx", providers=providers)
@@ -17,9 +21,9 @@ class ObjectDetection:
         self.classes = ["Vehicle", "Motor", "Bike","traffic light","traffic sign","pedestrian"]
         self.input_size = 800
 
+    # Convert frame to correct input format for yolo
     def preprocess_frame(self,frame):
         frame_w, frame_h = frame.shape[1], frame.shape[0]
-
         frame = cv2.resize(frame, (self.input_size, self.input_size))
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = frame.transpose(2, 0, 1)
@@ -29,46 +33,40 @@ class ObjectDetection:
         return frame, frame_w, frame_h
 
 
-    def get_objects(self, frame, conf_threshold=0.25):
+    # Get the actual yolo predictions
+    def get_objects(self, frame, frame_w, frame_h,conf_threshold=0.25):
+        # Pass data through model
         outputs = self.session.run(None, {self.input_name: frame})
         predictions = outputs[0].transpose()
 
+        # Conf scores
+        scores, class_ids=predictions[:,4:].max(dim=1)
 
-        boxes, scores, class_ids = [], [], []
+        # Filter out boxes with low conf scores
+        # And get coordinates and class corresp. to conf scores
+        filtered_indices=torch.nonzero(scores > conf_threshold).squeeze(1)
+        boxes=predictions[filtered_indices,:4]
+        class_ids=class_ids[filtered_indices]
+        scores=scores[filtered_indices]
 
-        # Filter predictions
-        for pred in predictions:
-            pred = pred.squeeze()
-            if len(pred[4:])==0:
-                continue
-            class_id = pred[4:].argmax()
-            conf = pred[4:].max()
-            if conf < conf_threshold:
-                continue
-            x_center, y_center, w, h = pred[:4].astype(float)
-            boxes.append((x_center, y_center, w, h))
-            scores.append(conf)
-            class_ids.append(class_id)
+        # Scale the boxes back to carla
+        scaling_kernel = torch.tensor([frame_w, frame_h, frame_w, frame_h]) / self.input_size
+        boxes = boxes * scaling_kernel
 
-        return boxes, scores, class_ids
+        # Coordinate extraction
+        x1 = boxes[:, 0] - boxes[:, 2] / 2
+        y1 = boxes[:, 1] - boxes[:, 3] / 2
+        x2 = boxes[:, 0] + boxes[:, 2] / 2
+        y2 = boxes[:, 1] + boxes[:, 3] / 2
+        box_coordinates=torch.stack([x1, y1, x2, y2], dim=1)
 
-    def rescale_boxes(self, boxes,frame_w,frame_h):
-        rescaled_boxes = []
-        for box in boxes:
-            x_center, y_center, w, h = box
+        # Remove overlapping boxes: boxes referring to same object
+        # NMS: https://docs.pytorch.org/vision/main/generated/torchvision.ops.nms.html
+        non_overlapping_boxes_indices = torchvision.ops.nms(box_coordinates,scores,iou_threshold=0.5)
+        return box_coordinates[non_overlapping_boxes_indices], class_ids[non_overlapping_boxes_indices], scores[non_overlapping_boxes_indices]
 
-            x_center = x_center / self.input_size * frame_w
-            y_center = y_center / self.input_size * frame_h
-            w = w / self.input_size * frame_w
-            h = h / self.input_size * frame_h
-            x1 = x_center - w / 2
-            y1 = y_center - h / 2
-            x2 = x_center + w / 2
-            y2 = y_center + h / 2
 
-            rescaled_boxes.append((x1, y1, x2, y2))
-        return rescaled_boxes
-
+    # Draw boxes onto the frame
     def add_object_boxes(self, frame, boxes, scores, class_ids):
         for (x1,y1,x2,y2), cls_id, score in zip(boxes, class_ids, scores):
             x1, y1, x2, y2 = map(int, [x1, y1, x2, y2])
@@ -78,11 +76,11 @@ class ObjectDetection:
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         return frame
 
+
     def detect_and_add_overlay(self,frame):
         pro_frame, frame_w, frame_h = self.preprocess_frame(frame)
-        boxes, scores, class_ids = self.get_objects(pro_frame)
-        rescaled_boxes = self.rescale_boxes(boxes,frame_w,frame_h)
-        frame = self.add_object_boxes(frame, rescaled_boxes, scores, class_ids)
+        boxes, scores, class_ids = self.get_objects(pro_frame,frame_w,frame_h)
+        frame = self.add_object_boxes(frame, boxes, scores, class_ids)
         frame_with_boxes_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         return frame_with_boxes_bgr
 
