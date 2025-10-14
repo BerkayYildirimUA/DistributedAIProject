27b68221da61254bbb30d9b48a1d750360127ce3
BerkayYildirimUA <Berkay.Yildirim@student.uantwerpen.be>
2025-10-14 18:21:09 +0200

    implmented the abstract classes

--- ACC/abstractions.py
+++ ACC/abstractions.py
@@ -6,16 +6,27 @@ import carla
 class VehicleState:
     speed: float
     speed_limit: float
-    distance_to_lead: float
+    distance_to_lead: float #car in front of the Ego
     safe_following_distance: float
 
 
 class StateSensor(ABC):
     """Class that returns the state of the world. Could be made to use sensors data in the future, but for now just calc via carla's build in methods"""
+
+    @abstractmethod
+    def get_state(self) -> VehicleState:
+        """get state of the car"""
+        pass
+
+
     pass
 
 class DecisionAgent(ABC):
     """Classes to make driving decisions. steering will be done by auto pilote, but acc will later be done with RL"""
+
+    @abstractmethod
+    def make_decision(self) -> carla.VehicleControl:
+        """get the next actions"""
     pass
 
 class UI(ABC):
