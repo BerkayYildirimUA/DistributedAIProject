c56d940a799ed980a49fdd528ae944b7c7df7311
BerkayYildirimUA <Berkay.Yildirim@student.uantwerpen.be>
2025-10-22 09:45:27 +0200

    refactoring

--- ACC/MyAgtents/ProjectAgent.py
+++ ACC/MyAgtents/ProjectAgent.py
@@ -1,1 +1,18 @@
-from ACC.PythonAPI.carla.agents import B
+from ACC.PythonAPI.carla.agents.navigation.behavior_agent import BehaviorAgent
+from ACC.Utils.abstractions import *
+
+class ProjectAgent(BehaviorAgent):
+    def __init__(self, vehicle, behavior='normal', opt_dict={}, map_inst=None, grp_inst=None, decision_agent: DecisionAgent = None):
+        super().__init__(vehicle, behavior, opt_dict, map_inst, grp_inst)
+        if decision_agent is None:
+            raise Exception("No DecisionAgent is None")
+
+        self.__decision_agent = decision_agent
+
+
+
+
+    def run_step(self, debug=False):
+        nav_control = super().run_step(debug)
+
+        return self.__decision_agent.make_decision(nav_control)
