1c316d504b2538bc9e1eb6640afc46b84e4002e3
WarreH <warrehofmans@gmail.com>
2025-10-30 10:49:27 +0100

    bug fix

--- app/engine/bird_eye_visualiser.py
+++ app/engine/bird_eye_visualiser.py
@@ -27,7 +27,9 @@ class BirdVisualiser:
 
     # Helper function to warp points
     def warp_points(self,pts):
+
         pts = np.array([pts], dtype='float32')
+        print(pts.shape)
         warped = cv2.perspectiveTransform(pts, self.M)[0]
         return warped
 
@@ -41,10 +43,10 @@ class BirdVisualiser:
         return object_middle_points,colors
     def generate_new_coords_with_colors(self,boxes,class_ids,lane_l,lane_r):
         # Warp everything
-        lane_l = self.warp_points(np.array(lane_l).reshape(-1, 1, 2))
-        lane_r = self.warp_points(np.array(lane_r).reshape(-1, 1, 2))
+        lane_l = self.warp_points(lane_l)
+        lane_r = self.warp_points(lane_r)
         objects,colors =self.get_object_coords_and_colors(boxes,class_ids)
-        objects = self.warp_points(np.array(objects).reshape(-1, 1, 2))
+        objects = self.warp_points(objects)
         lane_colors=["k"]*len(lane_l)+["k"]*len(lane_r)
         return [lane_l,lane_r,objects],[colors,lane_colors]
 
