d7b118f12e8bb2a498dbda81ca459cba7e78b786
WarreH <warrehofmans@gmail.com>
2025-10-30 12:10:42 +0100

    test

--- app/engine/bird_eye_visualiser.py
+++ app/engine/bird_eye_visualiser.py
@@ -52,7 +52,7 @@ class BirdVisualiser:
         for i,box in enumerate(boxes):
             if class_ids[i].item() in [0,1,2,3,5]:
                 print("IN")
-                object_middle_points.append(([(box[2]+box[0])/2,(box[3]+box[1])/2]))
+                object_middle_points.append([(box[2]+box[0])/2,(box[3]+box[1])/2])
                 colors.append(self.class_colors[class_ids[i]])
         return object_middle_points,colors
 
