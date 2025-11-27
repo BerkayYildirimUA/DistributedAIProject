b1e80067c5a2524314f8524cb463fdc17ac889ac
WarreH <warrehofmans@gmail.com>
2025-11-27 12:01:52 +0100

    bug fix

--- ACC/Utils/Sensors.py
+++ ACC/Utils/Sensors.py
@@ -144,7 +144,7 @@ class CarlaVBWorldStateSensor(StateSensor):
 
         self.speed_limit = self.__ego.get_speed_limit()
 
-        distance= self.vehicle_distance_memory.read()
+        distance= self.vehicle_distance_memory.read()[0]
 
         if self.counter % 300 == 0:
             logging.info(f"speed: {ego_velocity_ms * 3.6}km/h, speed lim: {self.speed_limit} km/h, distance to nearest: {distance}m, safe dist: {safe_distance}m, CRASH: nvt")
--- app/memory/shared_memory.py
+++ app/memory/shared_memory.py
@@ -49,7 +49,7 @@ class DepthCameraMemory(SharedMemory):
 class VehicleDistanceMemory(SharedMemory):
     def __init__(self):
         filename = "VEHICLE_DISTANCE_MEMORY.dat"
-        shape = (1, 1)
+        shape = (1,)
         dtype = np.float32
         super().__init__(filename,shape,dtype)
 
