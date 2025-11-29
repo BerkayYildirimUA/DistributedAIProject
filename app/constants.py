4823f71bb80026d7db7c0d8589d8f21c7063e5be
elwauters <eli.wauters@student.uantwerpen.be>
2025-11-30 00:39:06 +0100

    fixed constant naming error

--- app/constants.py
+++ app/constants.py
@@ -30,5 +30,5 @@ QUEUE_MAXSIZE = 10
 # Filename constants
 ACTUAL_OBJECTS_IN_FRONT_COUNT_FILE = "actual_objects_in_front_count.gz"
 ESTIMATED_OBJECT_IN_FRONT_COUNT_FILE = "estimated_objects_in_front_count.gz"
-ACTUAL_VEHICLE_DISTANCE_IN_FRONT_FILE = "actual_objects_in_front_count.gz"
+ACTUAL_VEHICLE_DISTANCE_IN_FRONT_FILE = "actual_vehicle_distance_in_front.gz"
 ESTIMATED_VEHICLE_DISTANCE_IN_FRONT_FILE = "estimated_vehicle_distance_in_front.gz"
