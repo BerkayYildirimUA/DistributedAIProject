7e5732826bc41afa11e8ad37da725123f45446d5
WarreH <warrehofmans@gmail.com>
2025-12-09 12:34:05 +0100

    more logging

--- ACC/Utils/Sensors.py
+++ ACC/Utils/Sensors.py
@@ -42,7 +42,7 @@ class CarlaWorldStateSensor(StateSensor):
 
     def print_vehicle_state(self,state:VehicleState):
         logging.info(
-            f"speed: {state.speed_ms * 3.6:.2f}fkm/h, speed limit: {state.speed_limit_ms*3.6:.2f} km/h, distance to nearest: {state.lead_distance_m:.2f}m, safe dist: {state.safe_following_distance_m:.2f}m,traffic light color: {state.light_color}, traffic light distance: {state.light_dist_m:.2f}m, CRASH: nvt")
+            f"speed: {state.speed_ms * 3.6:.2f}fkm/h, speed limit: {state.speed_limit_ms*3.6:.2f} km/h, distance to nearest: {state.lead_distance_m:.2f}m, safe dist: {state.safe_following_distance_m:.2f}m,traffic light color: {state.light_color}, traffic light distance: {state.light_dist_m:.2f}m, CRASH: {state.hasCrashed}, g-forces:{state.g_force_ego}, {state.relative_speed_ms}, {state.light_speed_ms}")
 
     def cleanup(self):
 
