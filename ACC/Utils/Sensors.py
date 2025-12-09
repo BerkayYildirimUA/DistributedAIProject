0d8b90b322ea4d343490a3ff587710b45bc40134
WarreH <warrehofmans@gmail.com>
2025-12-09 12:46:42 +0100

    no inf values for lead distance

--- ACC/Utils/Sensors.py
+++ ACC/Utils/Sensors.py
@@ -201,6 +201,8 @@ class CarlaVBWorldStateSensor(CarlaWorldStateSensor):
 
         self.start_sensor_threads()
 
+        self.prev_lead_distance=250.0
+
     def cleanup(self):
         pass
 
@@ -219,23 +221,20 @@ class CarlaVBWorldStateSensor(CarlaWorldStateSensor):
 
         self.speed_limit = self._ego.get_speed_limit()
 
+
         distance= self.vehicle_distance_memory.read()
-        print("found distance",distance)
+        if np.isinf(distance[0]):
+            lead_distance=self.prev_lead_distance
+        else:
+            self.prev_lead_distance = distance[0]
+            lead_distance = distance[0]
+
+        print("found distance",lead_distance)
         
         # TODO: replace with computer vision based logic
-        # ego_transform = self._ego.get_transform()
-        # ego_loc = ego_transform.location
-        # ego_waypoint = self._world.get_map().get_waypoint(ego_loc, project_to_road=True, lane_type=carla.LaneType.Driving)
-        # target_light_actor = self._get_trafficlight(ego_waypoint)
         traffic_light_dist_m = self.min_dist
         traffic_light_color = LightColors.green
 
-
-        # Use the actor if found
-        # if target_light_actor:
-        #     dist_calc = lambda l: math.sqrt((l.x - ego_loc.x) ** 2 + (l.y - ego_loc.y) ** 2 + (l.z - ego_loc.z) ** 2)
-        #     traffic_light_dist_m = dist_calc(target_light_actor.get_location())
-        #     traffic_light_color = self._get_light_color_enum(target_light_actor.get_state())
 
         # TODO: get speed limit from computer vision
         speed_limit=self.speed_limit
@@ -265,12 +264,11 @@ class CarlaVBWorldStateSensor(CarlaWorldStateSensor):
         # ctrl.steer in [-1,1] => schaal naar rad
         steer_rad = -float(ctrl.steer) * constants.MAX_STEER_RAD
 
-        lead_distance = self.min_dist if np.isinf(distance[0])  else distance[0]
         state2 = super().get_state()
         state= VehicleState(
             speed_ms=ego_velocity_ms,
             speed_limit_ms=speed_limit/3.6,
-            lead_distance_m=state2.lead_distance_m,
+            lead_distance_m=lead_distance,
             safe_following_distance_m=safe_distance,
             hasCrashed=False,
             light_color=traffic_light_color,
