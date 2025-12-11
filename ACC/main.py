bae32c9830c1024406e366f24424bbfe2d7ee1c3
WarreH <warrehofmans@gmail.com>
2025-12-11 18:45:24 +0100

    testing

--- ACC/main.py
+++ ACC/main.py
@@ -54,7 +54,7 @@ def main_loop(args):
             use_traffic_signs=True
         )
 
-        decisionAgent = RLDecisionAgent(sensor_real, "251210_200048_TD3_Capella_chunk_12600.msh")
+        decisionAgent = RLDecisionAgent(sensor_real, "251210_001928_TD3_Aldebaran_chunk_7200.msh")
 
         crash_detected = False
         frames_after_crash = 0
