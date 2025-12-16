76b639c60f915256cc5158b6af79e284c432bd76
WarreH <warrehofmans@gmail.com>
2025-12-16 18:22:30 +0100

    bug fixing

--- ACC/Utils/Sensors.py
+++ ACC/Utils/Sensors.py
@@ -546,7 +546,7 @@ class CarlaVBWorldStateSensor(CarlaWorldStateSensor):
         state= VehicleState(
             speed_ms=ego_velocity_ms,
             speed_limit_ms=speed_limit/3.6,
-            lead_distance_m=min(lead_distance-2,0),
+            lead_distance_m=max(lead_distance-2,0),
             safe_following_distance_m=safe_distance+10,
             crash_intensity=0.0,
             light_color=traffic_light_color,
