482857910de2ed7b7667c6b8906d7a06a4fbaf45
WarreH <warrehofmans@gmail.com>
2025-12-17 11:41:23 +0100

    final

--- ACC/main.py
+++ ACC/main.py
@@ -42,7 +42,7 @@ maybe change action space center if need be, like 0 =! do nothing, perhabs.
 
 def main_loop(args):
     scene = Scenario('vehicle.tesla.model3', delta_seconds=args.delta_seconds,
-                     map_name=args.map, number_of_npc=20
+                     map_name=args.map, number_of_npc=0
                      , lead_car_bp_name='vehicle.tesla.model3'
                      )
     engine = Engine(args, scene)
